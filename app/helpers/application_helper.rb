36a86364dcd7fba28c06a9f53940d97042d588a3
Chad <cedie@hotmail.com>
2011-09-20 00:01:52 -0700

    corrected errors in hayloft menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
 module ApplicationHelper
-
-  # Return a title on a per-page basis.
-  def title
-    base_title = "The Maverick Saloon"
-    if @title.nil?
-      base_title
-    else
-      "#{base_title} | #{@title}"
-    end
-  end
 end
