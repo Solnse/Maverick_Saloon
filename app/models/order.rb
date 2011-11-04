2bcc108f9a23b45d80eb8989464730e687fb55ca
Chad <cedie@hotmail.com>
2011-11-04 01:56:12 -0700

    lots of css cleanup done

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
-  PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+  PAYMENT_TYPES = [ "Check", "Credit card", "Good Looks" ]
   validates :name, :address_line_1, :city, :zip_code, :email, presence: true
   validates :pay_type, inclusion: PAYMENT_TYPES
   
